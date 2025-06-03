82587efee212199fabdb804e5daa7f3de810689b
Sidney <sbatchelder@whoi.edu>
2025-06-03 15:53:45 -0400

    initial commit

---
+++ src/convert_onnx_batchsize.py
@@ -0,0 +1,51 @@
+import onnx
+
+def convert_onnx_batchsize(model_path: str, output_path: str, batch=None):
+    """
+    Automatically convert ONNX model batch size:
+      - If input model is static and batch=None -> make it dynamic
+      - If input model is dynamic and batch=int -> make it static
+      - If both are static -> change batch size
+      - If both are dynamic -> no change unless you specify a batch size
+
+    Parameters:
+        model_path (str): Path to input ONNX model.
+        output_path (str): Path to save converted ONNX model.
+        batch (int or None):
+            - None = convert to dynamic batch
+            - int = convert to fixed batch size
+    """
+    print()
+    model = onnx.load(model_path)
+
+    def is_dynamic_dim(dim):
+        return (dim.HasField("dim_param") and dim.dim_param != "") or \
+               (not dim.HasField("dim_value")) or \
+               (dim.HasField("dim_value") and dim.dim_value == 0)
+
+    def update_batch_dim(tensor, batch, axis=0):
+        dim = tensor.type.tensor_type.shape.dim[axis]
+
+        # Detect original type
+        original_is_dynamic = is_dynamic_dim(dim)
+        original = dim.dim_param if original_is_dynamic else dim.dim_value
+        print(f"Original batch dim for '{tensor.name}': {original} ({'dynamic' if original_is_dynamic else 'static'})")
+
+        # Determine conversion action
+        if batch is None:
+            if original_is_dynamic: return # skip
+            # Convert to dynamic
+            dim.dim_value = 0
+            dim.dim_param = "batch_size"
+        else:
+            # Convert to static
+            dim.dim_param = ""
+            dim.dim_value = int(batch)
+
+    for tensor in model.graph.input:
+        update_batch_dim(tensor, batch)
+    for tensor in model.graph.output:
+        update_batch_dim(tensor, batch)
+
+    onnx.save(model, output_path)
+    print(f"converted model saved to: {output_path}")
---
+++ src/infer.py
@@ -0,0 +1,208 @@
+import json
+import os
+import argparse
+import datetime as dt
+
+from tqdm import tqdm
+import numpy as np
+import onnxruntime as ort
+import ifcb
+
+# set import paths to project root
+if __name__ == '__main__':
+    import sys, pathlib
+    PROJECT_ROOT = pathlib.Path(__file__).parent.parent.absolute()
+    if sys.path[0] != str(PROJECT_ROOT): sys.path.insert(0, str(PROJECT_ROOT))
+
+from datasets import IfcbBinImageTransformer, DataLoader, IfcbBinDataset
+
+# if torch not being imported/installed, do:
+# pip install onnxruntime-gpu[cuda,cudnn]
+ort.preload_dlls(directory="")
+# see https://onnxruntime.ai/docs/execution-providers/CUDA-ExecutionProvider.html#preload-dlls
+
+def argparse_init(parser=None):
+    if parser is None:
+        parser = argparse.ArgumentParser(
+            description='Train, Run, and perform other tasks related to ifcb and general image classification!')
+
+    ## Run Vars ##
+    parser.add_argument('MODEL', help='Path to a previously-trained model file')
+    parser.add_argument('BINS', nargs='+', help='Resource(s) to be classified. Accepts a bin, an image, a text-file, or a directory. Directories are accessed recursively')
+    parser.add_argument('--batch', '-b', type=int, help='Specify inference batchsize (for dynamically-batched MODEL only)')
+    #parser.add_argument('--type', dest='src_type', default='bin', choices=['bin','img'], help='File type to perform classification on. Defaults is "bin"')
+    parser.add_argument('--classes', help='Path to row-delimited classlist file')
+    parser.add_argument('--outdir', default='./output/{RUN_DATE}', help='Default is "./output/{RUN_DATE}')
+    parser.add_argument('--outfile', default='{BIN_ID}.csv', help='Default is "{BIN_ID}.csv"')
+    #    help='''Name/pattern of the output classification file.
+    #            If TYPE==bin, files are created on a per-bin basis. OUTFILE must include "{BIN_ID}", which will be replaced with the a bin's id.
+    #            A few patters are recognized: {BIN_ID}, {BIN_YEAR}, {BIN_DATE}, {INPUT_SUBDIRS}.
+    #            A few output file formats are recognized: .json, .mat, and .h5 (hdf).
+    #            Default for TYPE==bin is "D{BIN_YEAR}/D{BIN_DATE}/{BIN_ID}_class.h5"; Default for TYPE==img is "img_results.json".
+    #         ''')
+
+    return parser
+
+
+def argparse_runtime_args(args):
+    # Record Timestamp
+    args.cmd_timestamp = dt.datetime.now(dt.timezone.utc).isoformat(timespec='seconds')
+    args.run_date_str, args.run_time_str = args.cmd_timestamp.split('T')
+
+    # Record GPUs
+    args.gpus = [int(gpu) for gpu in os.environ.get('CUDA_VISIBLE_DEVICES', '').split(',')]
+
+    # Format Output Directory
+    #args.outdir = args.outdir.format(RUN_DATE=run_date_str)
+
+    # set OUTFILE defaults
+    #if not args.outfile:
+    #    if args.src_type == 'bin': args.outfile=['D{BIN_YEAR}/D{BIN_DATE}/{BIN_ID}_class.h5']
+    #    if args.src_type == 'img': args.outfile = ['img_results.json']
+
+    # read in classes from file, one class per line
+    if args.classes and os.path.isfile(args.classes):
+        with open(args.classes) as f:
+            args.classes = f.read().strip().splitlines()
+
+    bins = []
+    for bin_thing in args.BINS:
+        if os.path.isdir(bin_thing):
+            dd = ifcb.DataDirectory(bin_thing, blacklist=('bad','skip','beads','temp','data_temp'))
+            bins.extend( [binobj.fileset.basepath for binobj in dd] )
+        elif bin_thing.endswith('.txt') or bin_thing.endswith('.list'):  # TODO TEST: textfile bin run
+            with open(bin_thing, 'r') as f:
+                bin_list_from_file = f.read().splitlines()
+            bins.extend(bin_list_from_file)
+        else:
+            bins.append(bin_thing)
+    args.BINS = bins
+
+'''
+    if not args.clobber:
+        output_files = [os.path.join(args.outdir, ofile) for ofile in args.outfile]
+        outfile_dict = dict(BIN_ID=bin_obj.pid,
+                            BIN_YEAR=bin_obj.year,
+                            BIN_DATE=bin_obj.yearday,
+                            INPUT_SUBDIRS=bin_obj.namespace)
+        output_files = [ofile.format(**outfile_dict).replace(2*os.sep,os.sep) for ofile in output_files]
+        if all([ os.path.isfile(ofile) for ofile in output_files ]):
+            print('{} result-file(s) already exist - skipping this bin'.format(bin_obj))
+            continue
+
+'''
+
+def softmax(x, axis=None):
+    x_max = np.amax(x, axis=axis, keepdims=True)
+    exp_x_shifted = np.exp(x - x_max)
+    return exp_x_shifted / np.sum(exp_x_shifted, axis=axis, keepdims=True)
+
+
+def pad_batch(batch:np.ndarray, target_batch_size:int):
+    """
+    Pads the input batch to target_batch_size by repeating the last element.
+
+    Args:
+        batch (np.ndarray): Input batch of shape (N, C, H, W)
+        target_batch_size (int): Expected batch size by the model
+
+    Returns:
+        np.ndarray: Padded batch of shape (target_batch_size, C, H, W)
+    """
+    current_size = batch.shape[0]
+    if current_size == target_batch_size:
+        return batch
+    elif current_size > target_batch_size:
+        raise ValueError(f"Batch size {current_size} exceeds target size {target_batch_size}")
+
+    pad_size = target_batch_size - current_size
+    pad_shape = (pad_size,) + batch.shape[1:]
+    pad = np.zeros(pad_shape, dtype=batch.dtype)
+    padded_batch = np.concatenate([batch, pad], axis=0)
+    return padded_batch
+
+
+def write_output(args, bin_id, pids, score_matrix):
+    outpath = os.path.join(args.outdir, args.outfile)
+    outpath = outpath.format(RUN_DATE=args.run_date_str, BIN_ID=bin_id)
+
+    with open(outpath, 'w') as f:
+        if args.classes:
+            f.write(','.join(['pid']+args.classes)+'\n')
+        for pid, score_row in zip(pids, score_matrix):
+            str_row = ','.join(map(str,[pid]+score_row.tolist()))
+            f.write(str_row+'\n')
+    print(f'{bin_id} Scores written to {outpath}')
+
+
+def main(args):
+
+    # load model
+    providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
+    sess_options = ort.SessionOptions()
+    ort_session = ort.InferenceSession(args.MODEL, sess_options=sess_options, providers=providers)
+
+    input0 = ort_session.get_inputs()[0]
+    model_batch = input0.shape[0]
+    img_size = input0.shape[-1]
+    input_type = input0.type  # str, eg "tensor(float16)"
+    input_type = getattr(np, input_type[7:-1]) if input_type!="tensor(float)" else np.float32
+
+    dynamic_batching = True
+    if isinstance(model_batch, str):  # dynamic
+        assert args.batch is not None, 'Must specify inference batch size for dynamically batched MODEL'
+        inference_batchsize = args.batch
+    else:
+        assert args.batch is None or model_batch==args.batch, 'MODEL is statically batched, inference batch size cannot be adjusted'
+        dynamic_batching = False
+        inference_batchsize = model_batch
+
+    # initialize dataset
+    transformer = IfcbBinImageTransformer(img_size, dtype=input_type)
+    pbar = tqdm(args.BINS, desc=f'batchsize={inference_batchsize}', unit='bins')
+    for bin_accessor in pbar:
+        img_pids = []
+        score_matrix = None
+        dataset = IfcbBinDataset(bin_accessor)
+        dataloader = DataLoader(dataset, inference_batchsize, transformer)
+
+        # do inference
+        for batch_tuple in dataloader:
+            batch,batch_pids = batch_tuple[0], batch_tuple[1]
+            size_of_batch = batch.shape[0]
+
+            if dynamic_batching or size_of_batch == inference_batchsize:
+                outputs = ort_session.run(None, {input0.name:batch})
+            else:
+                #print('Last batch difference:', inference_batchsize-size_of_batch)
+                batch = pad_batch(batch, inference_batchsize)
+                outputs = ort_session.run(None, {input0.name:batch})
+                outputs = [output[:size_of_batch] for output in outputs]
+
+            batch_score_matrix = outputs[0]
+            batch_score_matrix = softmax(batch_score_matrix, axis=1)
+            if score_matrix is None:
+                score_matrix = batch_score_matrix
+            else:
+                score_matrix = np.concatenate([score_matrix, batch_score_matrix], axis=0)
+            img_pids.extend(batch_pids)
+
+        # write a score matrix csv for each bin
+        write_output(args, dataset.pid, img_pids, score_matrix)
+        #output_classes = np.argmax(score_matrix, axis=1)
+        #output_scores = np.max(score_matrix, axis=1)
+
+
+if __name__ == '__main__':
+    parser = argparse_init()
+    args = parser.parse_args()
+    argparse_runtime_args(args)
+    main(args)
+
+
+
+
+
+
+
+
